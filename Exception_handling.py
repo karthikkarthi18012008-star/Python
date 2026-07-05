2291effa7d6dd8f3811e7c39b909f1741d1c3f51
Karthik <karthikkarthi18012008@gmail.com>
2026-07-05 18:51:17 +0530

    update exception handling code

--- Exception_handling.py
+++ Exception_handling.py
@@ -74,3 +74,36 @@ except Exception as ex:
     print(ex)
 else:
     print(f"the result is {result}")
+
+## try,except,else and finally
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+except ValueError:
+    print("That's not a valid number!")
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except Exception as ex:
+    print(ex)
+else:
+    print(f"The result is {result}")
+finally:
+    print("Execution complete.")
+
+### File handling and Exception HAndling
+
+try:
+    file=open('example1.txt','r')
+    content=file.read()
+    a=b
+    print(content)
+
+except FileNotFoundError:
+    print("The file does not exists")
+except Exception as ex:
+    print(ex)
+
+finally:
+    if 'file' in locals() or not file.closed():
+        file.close()
+        print('file close')
