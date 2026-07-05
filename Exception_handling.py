0065e955d46b224db448492b5d354c7b81387c84
Karthik <karthikkarthi18012008@gmail.com>
2026-07-05 18:41:58 +0530

    update exception handling code

--- Exception_handling.py
+++ Exception_handling.py
@@ -15,7 +15,7 @@ except ZeroDivisionError:
 except ValueError:
     print("Error: Invalid input. Please enter a valid number.")
 
-#using finallu block
+#using finally block
 try:
     file = open("sample.txt","r")
     print(file.read())
@@ -24,7 +24,53 @@ except FileNotFoundError:
 finally:
     print("Closing file... (even if error occurred)")
 
-             
-        
-    
+#using try,except
+try:
+    a = b
+except NameError as ex:
+    print(ex)
 
+             
+try:
+    a = int(input("enter a number:"))
+    result = 2/a
+except ZeroDivisionError as ex:
+    print(ex)
+    print("enter a valid denomenator:")
+
+
+try:
+    result = 2/1
+    a=b
+except ZeroDivisionError as ex:
+    print(ex)
+    print("enter a valid denomenator:")
+except Exception as ex1:
+    print(ex1)
+    print("main exception got caought here")
+
+
+#multiple exceptions
+
+try:
+    num=int(input("Enter a number"))
+    result=10/num
+except ValueError:
+    print("This is not a valid number")
+except ZeroDivisionError:
+    print("enter denominator greater than 0")
+except Exception as ex:
+    print(ex)
+    
+## try,except,else block
+try:
+    num=int(input("Enter a number:"))
+    result=10/num
+except ValueError:
+    print("That's not a valid number!")
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except Exception as ex:
+    print(ex)
+else:
+    print(f"the result is {result}")
