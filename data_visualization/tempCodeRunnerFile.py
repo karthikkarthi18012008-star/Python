6662f7330580635eee9443f2091a511bbb1e50ed
Karthik <karthikkarthi18012008@gmail.com>
2026-07-09 11:27:00 +0530

    handling imbalanced datasets

--- data_visualization/tempCodeRunnerFile.py
+++ data_visualization/tempCodeRunnerFile.py
@@ -1,14 +1,30 @@
+from sklearn.datasets import make_classification
 
-## CREATE MY DATAFRAME WITH IMBALANCED DATASET
-class_0 = pd.DataFrame({
-    'feature_1': np.random.normal(loc=0, scale=1, size=n_class_0),
-    'feature_2': np.random.normal(loc=0, scale=1, size=n_class_0),
-    'target': [0] * n_class_0
-})
+x,y = make_classification(n_redundant=0,n_features=2,n_clusters_per_class=1,weights=[0.90],n_samples=1000,random_state=12)
+import pandas as pd
+df1 = pd.DataFrame(x,columns = ['f1','f2'])
+print(df1)
+print('='*90)
+df2 = pd.DataFrame(y,columns = ['target'])
+print(df2)
+print('='*90)
+final_df=pd.concat([df1,df2],axis=1)
+print(final_df)
+print('='*90)
 
-class_1 = pd.DataFrame({
-    'feature_1': np.random.normal(loc=2, scale=1, size=n_class_1),
-    'feature_2': np.random.normal(loc=2, scale=1, size=n_class_1),
-    'target': [1] * n_class_1
-})
-print(class_0['feature_1'])
+import matplotlib.pyplot as plt
+plt.scatter(final_df['f1'],final_df['f2'],c = final_df['target'])
+plt.show()
+
+from imblearn.over_sampling import SMOTE
+
+oversample = SMOTE()
+X,y=oversample.fit_resample(final_df[['f1','f2']],final_df['target'])
+print(len(y[y==0]))
+print(len(y[y==1]))
+
+df1=pd.DataFrame(X,columns=['f1','f2'])
+df2=pd.DataFrame(y,columns=['target'])
+oversample_df=pd.concat([df1,df2],axis=1)
+plt.scatter(oversample_df['f1'],oversample_df['f2'],c = oversample_df['target'])
+plt.show()
