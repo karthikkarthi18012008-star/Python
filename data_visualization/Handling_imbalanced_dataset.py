543ed72b1f2540162cffa9f11ceb454883c6ec5b
Karthik <karthikkarthi18012008@gmail.com>
2026-07-09 11:19:06 +0530

    smote code

--- data_visualization/Handling_imbalanced_dataset.py
+++ data_visualization/Handling_imbalanced_dataset.py
@@ -84,3 +84,36 @@ print(df_downsampled['target'].value_counts())
 # | Duplicate samples | Yes (random oversampling)                                      | No                                                                   |
 # | Risk              | Can overfit due to repeated samples                            | Can underfit if too much data is removed                             |
 # | Best for          | Small minority class when you want to retain all majority data | Very large datasets where losing some majority samples is acceptable |
+
+
+#SMOTE
+from sklearn.datasets import make_classification
+
+x,y = make_classification(n_redundant=0,n_features=2,n_clusters_per_class=1,weights=[0.90],n_samples=1000,random_state=12)
+import pandas as pd
+df1 = pd.DataFrame(x,columns = ['f1','f2'])
+print(df1)
+print('='*90)
+df2 = pd.DataFrame(y,columns = ['target'])
+print(df2)
+print('='*90)
+final_df=pd.concat([df1,df2],axis=1)
+print(final_df)
+print('='*90)
+
+import matplotlib.pyplot as plt
+plt.scatter(final_df['f1'],final_df['f2'],c = final_df['target'])
+plt.show()
+
+from imblearn.over_sampling import SMOTE
+
+oversample = SMOTE()
+X,y=oversample.fit_resample(final_df[['f1','f2']],final_df['target'])
+print(len(y[y==0]))
+print(len(y[y==1]))
+
+df1=pd.DataFrame(X,columns=['f1','f2'])
+df2=pd.DataFrame(y,columns=['target'])
+oversample_df=pd.concat([df1,df2],axis=1)
+plt.scatter(oversample_df['f1'],oversample_df['f2'],c = oversample_df['target'])
+plt.show()
